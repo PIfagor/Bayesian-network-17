a43f696f9b57edae428bf5ec5528dd548eb223ac
Propoy Yaroslav <yaroslavpropoy@gmail.com>
2017-04-11 22:03:24 +0300

    Implement Infer.NET library

--- Assets/Scripts/Models/Symptom.cs
+++ Assets/Scripts/Models/Symptom.cs
@@ -1,16 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿namespace YPGraphCore
+{
 
-public class Symptom : MonoBehaviour {
+    public class Symptom
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
